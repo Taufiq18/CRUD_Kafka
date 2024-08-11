d4b8d8e9f8b736166ffd2a07bfefc1589b50cdcd
UriahBlake <180501taufiq@gmail.com>
2024-08-12 03:16:01 +0700

    first commit

---
+++ src/test/java/com/app/crudKafka/CrudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.crudKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
